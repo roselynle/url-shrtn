df0e7d3694b67c090cda2bcee0f2d3bfb5b01d92
Roselyn Le <roselynle95@gmail.com>
2021-05-11 15:08:16 +0100

    add db config

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
