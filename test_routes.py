7c3be10105cf3a1d1809086d36d5a89e4218ef6a
mfy <Faisaly1234@outlook.com>
2021-05-12 11:51:52 +0100

    updated test

--- test_routes.py
+++ test_routes.py
@@ -1,26 +1,38 @@
 import json
 
+
 def test_home(api):
-    resp = api.get('/')
+    resp = api.get('/') 
+    print(resp.data)
     assert resp.status == '200 OK'
-    assert b'Enter your URL' in resp.data
 
-
-# def test_post_url(api):
-#     form_data = {'}
-#     resp = api.post('/', data=form_data)
-#     assert resp.status == '200 OK'
-
+def test_display_short_url(api):
+    resp = api.get("/display/test1")
+    assert resp.status_code == 200
+    assert b'Here is your shortened URL:' in resp.data
 
 
 def test_404_handling(api):
     resp = api.get('/tgy')
     assert resp.status == '404 NOT FOUND'
+    assert b'You seem to be a bit lost!' in resp.data
 
 
 def test_405_handling(api):
     resp = api.post('/tgy')
     assert resp.status == '405 METHOD NOT ALLOWED'
+    assert b'That\'s Forbidden!' in resp.data
+
+# def test_post_url(api):
+#     form = {'url':'https://stackoverflow.com'}
+#     resp = api.post('/', data=form)
+#     assert resp.status == '200 OK'
+
+
+
+# def test_short_url(api):
+#     resp = api.get("/dii")
+#     assert resp.status_code == 200
 
     
 
